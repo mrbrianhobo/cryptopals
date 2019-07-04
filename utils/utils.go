267a19e353a4abe7de1d930947c30358d87659c8
Brian Ho <mr.brianhobo@gmail.com>
2019-07-03 22:14:37 -0700

    add hex/base64 encoding/decoding utils

---
+++ utils/utils.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"log"
+
+	"encoding/base64"
+	"encoding/hex"
+)
+
+func HexToBytes(hexString string) []byte {
+	ciphertext, err := hex.DecodeString(hexString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return ciphertext
+}
+
+func Base64ToBytes(base64String string) []byte {
+	ciphertext, err := base64.StdEncoding.DecodeString(base64String)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return ciphertext
+}
+
+func BytesToHex(src []byte) string {
+	return hex.EncodeToString(src)
+}
+
+func BytesToBase64(src []byte) string {
+	return base64.StdEncoding.EncodeToString(src)
+}
